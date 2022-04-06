a269ed46c966827c663bebe9f99afd9b8a9d9701
HNChing1998 <Hosobanaoki.ching@dnsc.edu.ph>
2022-04-06 16:03:55 +0800

    Initial Commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="app">
+  
+  <router-view></router-view>
+  <!--<LoginForm/>-->
+  <!--<SignUp/>-->
+</div> 
+</template>
+<script>
+
+
+//import LoginForm from './components/LoginForm.vue';
+//import SignUp from './components/SignUp.vue';
+export default {
+  components:{  
+    //LoginForm,
+    //SingUp
+   
+  },
+  data() 
+  {
+    return{
+      showPassword: false
+    }
+  }
+}
+</script>
